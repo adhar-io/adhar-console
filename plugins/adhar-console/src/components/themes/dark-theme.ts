709f74482ac4b6ac605f87ba625a98e5316b6af6
Tapas Jena <tapas.friends@gmail.com>
2024-11-01 19:41:12 +0800

    theme updated

--- plugins/adhar-console/src/components/themes/dark-theme.ts
+++ plugins/adhar-console/src/components/themes/dark-theme.ts
@@ -11,35 +11,50 @@ export const adharDarkTheme = createUnifiedTheme({
     palette: {
       ...palettes.dark,
       primary: {
-        main: '#25a0c2',
+        main: '#6366f1',
       },
       secondary: {
-        main: '#00568c',
+        main: '#6366f1',
       },
     },
   }),
   defaultPageTheme: 'home',
   pageTheme: {
-    home: genPageTheme({colors: ['#25a0c2', '#00568c'], shape: shapes.wave}),
+    home: genPageTheme({
+      colors: ['#6366f1', '#6366f1'], 
+      shape: shapes.wave2
+    }),
     documentation: genPageTheme({
-      colors: ['#25a0c2', '#00568c'],
+      colors: ['#6366f1', '#6366f1'],
       shape: shapes.wave2,
     }),
-    tool: genPageTheme({colors: ['#25a0c2', '#00568c'], shape: shapes.round}),
+    tool: genPageTheme({
+      colors: ['#6366f1', '#6366f1'], 
+      shape: shapes.round
+    }),
     service: genPageTheme({
-      colors: ['#25a0c2', '#00568c'],
-      shape: shapes.wave,
+      colors: ['#6366f1', '#6366f1'],
+      shape: shapes.wave2,
     }),
     website: genPageTheme({
-      colors: ['#25a0c2', '#00568c'],
-      shape: shapes.wave,
+      colors: ['#6366f1', '#6366f1'],
+      shape: shapes.wave2,
     }),
     library: genPageTheme({
-      colors: ['#25a0c2', '#00568c'],
-      shape: shapes.wave,
+      colors: ['#6366f1', '#6366f1'],
+      shape: shapes.wave2,
     }),
-    other: genPageTheme({colors: ['#25a0c2', '#00568c'], shape: shapes.wave}),
-    app: genPageTheme({colors: ['#25a0c2', '#00568c'], shape: shapes.wave}),
-    apis: genPageTheme({colors: ['#25a0c2', '#00568c'], shape: shapes.wave}),
+    other: genPageTheme({
+      colors: ['#6366f1', '#6366f1'], 
+      shape: shapes.wave2
+    }),
+    app: genPageTheme({
+      colors: ['#6366f1', '#6366f1'], 
+      shape: shapes.wave2
+    }),
+    apis: genPageTheme({
+      colors: ['#6366f1', '#6366f1'], 
+      shape: shapes.wave2
+    }),
   },
 });
--- plugins/adhar-console/src/components/themes/light-theme.ts
+++ plugins/adhar-console/src/components/themes/light-theme.ts
@@ -10,36 +10,61 @@ export const adharLightTheme = createUnifiedTheme({
     palette: {
       ...palettes.light,
       primary: {
-        main: '#5A00B0',
+        main: '#6366f1',
       },
       secondary: {
-        main: '#00adee',
+        main: '#2dd4bf',
+      },
+      bursts: {
+        fontColor: '#fff',
+        slackChannelText: '#000',
+        backgroundColor: {
+          default: '#6366f1',
+        },
+        gradient: {
+          linear: 'linear-gradient(180deg, #6366f1 0%, #2dd4bf 100%)',
+        },
       },
     },
   }),
   defaultPageTheme: 'home',
   pageTheme: {
-    home: genPageTheme({colors: ['#5A00B0', '#00adee'], shape: shapes.wave2}),
+    home: genPageTheme({
+      colors: ['#6366f1', '#6366f1'], 
+      shape: shapes.wave2
+    }),
     documentation: genPageTheme({
-      colors: ['#5A00B0', '#00adee'],
+      colors: ['#6366f1', '#6366f1'],
       shape: shapes.wave2,
     }),
-    tool: genPageTheme({colors: ['#5A00B0', '#00adee'], shape: shapes.round}),
+    tool: genPageTheme({
+      colors: ['#6366f1', '#6366f1'], 
+      shape: shapes.round
+    }),
     service: genPageTheme({
-      colors: ['#5A00B0', '#00adee'],
+      colors: ['#6366f1', '#6366f1'],
       shape: shapes.wave2,
     }),
     website: genPageTheme({
-      colors: ['#5A00B0', '#00adee'],
+      colors: ['#6366f1', '#6366f1'],
       shape: shapes.wave2,
     }),
     library: genPageTheme({
-      colors: ['#5A00B0', '#00adee'],
+      colors: ['#6366f1', '#6366f1'],
       shape: shapes.wave2,
     }),
-    other: genPageTheme({colors: ['#5A00B0', '#00adee'], shape: shapes.wave2}),
-    app: genPageTheme({colors: ['#5A00B0', '#00adee'], shape: shapes.wave2}),
-    apis: genPageTheme({colors: ['#5A00B0', '#00adee'], shape: shapes.wave2}),
+    other: genPageTheme({
+      colors: ['#6366f1', '#6366f1'], 
+      shape: shapes.wave2
+    }),
+    app: genPageTheme({
+      colors: ['#6366f1', '#6366f1'], 
+      shape: shapes.wave2
+    }),
+    apis: genPageTheme({
+      colors: ['#6366f1', '#6366f1'], 
+      shape: shapes.wave2
+    }),
   },
 });
 
